b0ee1b751fb3d9ab49183d00257f928998771a68
Jeneesha Priyadarshani <135360330+JENEESHAPRIYADARSHANI@users.noreply.github.com>
2023-10-23 13:03:41 +0530

    issue 1

---
+++ JENEESHAPRIYADARSHANI/issue 1/src/main/java/com/mycompany/gunsmith/Gunsmith.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.gunsmith;
+
+public class Gunsmith {
+
+    public static void main(String[] args)
+    {
+        int a ,l,p,snipe,rivolver;
+        NewJFrame G1=new NewJFrame();
+        G1.show();        
+    }
+}
